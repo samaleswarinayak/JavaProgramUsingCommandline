995474d755c905b95703b16557496ed466280d56
Samaleswari Prasad Nayak <33372052+samaleswarinayak@users.noreply.github.com>
2020-11-14 00:51:06 +0530

    Add files via upload
    Users can understand the procedure to execute programs by passing arguments through commandline in Java.

---
+++ TestCommandline.java
@@ -0,0 +1,13 @@
+/*Use of commandline argument for addition of two numbers in Java*/
+
+class TestCommandline
+{
+	public static void main(String args[])
+	{
+		int num1,num2,res;
+		num1 = Integer.parseInt(args[0]);
+		num2 = Integer.parseInt(args[1]);
+		res = num1 + num2;
+		System.out.println("Sum of "+num1+" and "+num2+" is "+res);
+	}
+}
---
+++ TestCommandlineArgument.java
@@ -0,0 +1,9 @@
+/*Use of commandline argument in Java*/
+
+class TestCommandlineArgument
+{
+	public static void main(String args[])
+	{
+		System.out.println("Number of arguments passed through commandline = "+args.length);
+	}
+}
